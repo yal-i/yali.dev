d87d66f207dd71b10f97e421393a7c79c3a8de32
yali <tangozyusufali@gmail.com>
2024-04-28 02:17:18 +0300

    style: update header

--- components/Header.vue
+++ components/Header.vue
@@ -21,10 +21,28 @@ const links = [
     to: "#contact",
   },
 ];
+
+const headerClass = ref("");
+
+onMounted(() => {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 110) {
+      headerClass.value =
+        "sticky top-0 left-0 right-0 z-50 border-b-[1px] border-zinc-800 backdrop-blur-3xl bg-[rgba(0,0,0,0.8)]";
+    } else {
+      headerClass.value = "";
+    }
+  });
+});
 </script>
 
 <template>
-  <header>
+  <header
+    :visible="{ opacity: 1, y: 0, transition: { duration: 1000 } }"
+    :initial="{ opacity: 0, y: -50 }"
+    :class="headerClass"
+    v-motion
+  >
     <UContainer class="py-7 flex items-center">
       <span
         class="text-black text-2xl font-extrabold uppercase mr-auto"
